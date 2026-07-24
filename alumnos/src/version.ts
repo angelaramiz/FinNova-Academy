cdc8b93482db3a64e4867c301e18ce5139b24f9c
Angel Ramirez <angel@example.com>
2026-07-24 04:30:23 -0600

    feat: 3 nuevos workflows contables, frontend v0.1.98

--- alumnos/src/version.ts
+++ alumnos/src/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit
-export const VERSION = '0.1.97';
-export const BUILD_HASH = '57c92fb';
+export const VERSION = '0.1.98';
+export const BUILD_HASH = '272284e';
 export const BUILD_BRANCH = 'main';
-export const BUILD_TIME = '2026-07-24T10:19:28.877Z';
-export const COMMIT_TIME = '2026-07-24T01:45:47-06:00';
+export const BUILD_TIME = '2026-07-24T10:29:54.534Z';
+export const COMMIT_TIME = '2026-07-24T04:19:55-06:00';
